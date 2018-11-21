a86774663515ea1b8e2c9dfa89d2a0ee1c74813d
李培帅_集团总部 <lipeishuai@www.lhwork.net>
2018-11-21 21:47:38 +0800

    Controller

--- longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/TaskListController.java
+++ longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/TaskListController.java
@@ -3,6 +3,7 @@ package com.longfor.longjian.houseqm.app.controller;
 import com.longfor.gaia.gfs.web.mock.MockOperation;
 import com.longfor.longjian.houseqm.app.vo.TaskListVo;
 import com.longfor.longjian.houseqm.app.vo.TaskResponse;
+import com.longfor.longjian.houseqm.app.vo.TaskRoleListVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * http://192.168.37.159:3000/project/8/interface/api/226  获取项目下任务列表任务信息
+ * http://192.168.37.159:3000/project/8/interface/api/280  获取任务角色列表
+ *
  * @author lipeishuai
  * @date 2018/11/17 15:07
  */
@@ -35,12 +38,36 @@ public class TaskListController {
      */
     @MockOperation
     @GetMapping(value = "list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public TaskResponse<TaskListVo> doAction(@RequestParam(value="project_id" ) Integer projectId,
+    public TaskResponse<TaskListVo> list(@RequestParam(value="project_id" ) Integer projectId,
                                          @RequestParam(value="category_cls") String categoryCls,
                                          @RequestParam(value="page_level") String pageLevel,
                                          @RequestParam(value="group_id") String groupId,
                                          @RequestParam(value="team_id") String teamId,
                                          @RequestParam(value="status") String status){
+
+
+        return null;
+    }
+
+    /**
+     * 获取任务角色列表
+     *
+     * @param projectId
+     * @param categoryCls
+     * @param pageLevel
+     * @param groupId
+     * @param teamId
+     * @param status
+     * @return
+     */
+    @MockOperation
+    @GetMapping(value = "task_role", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public TaskResponse<TaskRoleListVo> taskRole(@RequestParam(value="project_id" ) Integer projectId,
+                                                 @RequestParam(value="category_cls") String categoryCls,
+                                                 @RequestParam(value="page_level") String pageLevel,
+                                                 @RequestParam(value="group_id") String groupId,
+                                                 @RequestParam(value="team_id") String teamId,
+                                                 @RequestParam(value="status") String status){
 
 
         return null;
