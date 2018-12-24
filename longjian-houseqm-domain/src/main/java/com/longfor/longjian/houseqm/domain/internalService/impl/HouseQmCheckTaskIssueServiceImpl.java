a26a838c9de363afdf64bf5659ce2f3d8f9fa13f
houyan <615786504@qq.com>
2018-12-24 14:46:28 +0800

    Merge branch 'master' of http://git.longhu.net/longjian_v3/longjian-houseqm-server
    # Conflicts:
    #	longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/HouseqmStatisticController.java
    #	longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/service/IHouseqmStatisticService.java
    #	longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/service/impl/HouseqmStatisticServiceImpl.java
    #	longjian-houseqm-common/src/main/java/com/longfor/longjian/houseqm/util/DateUtil.java
    #	longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/HouseQmCheckTaskIssueMapper.java
    #	longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/domain/internalService/HouseQmCheckTaskIssueService.java
    #	longjian-houseqm-domain/src/main/resources/com/longfor/longjian/houseqm/dao/HouseQmCheckTaskIssueMapper.xml

--- longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/domain/internalService/impl/HouseQmCheckTaskIssueServiceImpl.java
+++ longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/domain/internalService/impl/HouseQmCheckTaskIssueServiceImpl.java
@@ -1,17 +1,15 @@
 package com.longfor.longjian.houseqm.domain.internalService.impl;
 
 import com.longfor.gaia.gfs.data.mybatis.datasource.LFAssignDataSource;
-import com.longfor.longjian.houseqm.dao.HouseQmCheckTaskIssueMapper;
+import com.longfor.longjian.houseqm.dao.*;
 import com.longfor.longjian.houseqm.domain.internalService.HouseQmCheckTaskIssueService;
 import com.longfor.longjian.houseqm.po.*;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
