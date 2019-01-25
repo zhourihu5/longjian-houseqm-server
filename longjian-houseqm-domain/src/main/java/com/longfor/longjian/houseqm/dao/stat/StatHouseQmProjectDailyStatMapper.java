331d0672fbf660b1fae8c6e16cf8130e928e4c40
李培帅_集团总部 <lipeishuai@www.lhwork.net>
2019-01-25 11:03:33 +0800

    stat mapper 重新生成

--- longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/stat/StatHouseQmProjectDailyStatMapper.java
+++ longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/stat/StatHouseQmProjectDailyStatMapper.java
@@ -2,6 +2,11 @@ package com.longfor.longjian.houseqm.dao.stat;
 
 import com.longfor.gaia.gfs.data.mybatis.LFMySQLMapper;
 import com.longfor.longjian.houseqm.po.stat.StatHouseQmProjectDailyStat;
+import com.longfor.longjian.houseqm.po.stat.StatHouseQmProjectDailyStatExample;
+import java.util.List;
 
 public interface StatHouseQmProjectDailyStatMapper extends LFMySQLMapper<StatHouseQmProjectDailyStat> {
+    int countByExample(StatHouseQmProjectDailyStatExample example);
+
+    List<StatHouseQmProjectDailyStat> selectByExample(StatHouseQmProjectDailyStatExample example);
 }
--- longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/stat/StatHouseQmProjectUserWorkloadCheckerDailyStatMapper.java
+++ longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/stat/StatHouseQmProjectUserWorkloadCheckerDailyStatMapper.java
@@ -2,6 +2,11 @@ package com.longfor.longjian.houseqm.dao.stat;
 
 import com.longfor.gaia.gfs.data.mybatis.LFMySQLMapper;
 import com.longfor.longjian.houseqm.po.stat.StatHouseQmProjectUserWorkloadCheckerDailyStat;
+import com.longfor.longjian.houseqm.po.stat.StatHouseQmProjectUserWorkloadCheckerDailyStatExample;
+import java.util.List;
 
 public interface StatHouseQmProjectUserWorkloadCheckerDailyStatMapper extends LFMySQLMapper<StatHouseQmProjectUserWorkloadCheckerDailyStat> {
+    int countByExample(StatHouseQmProjectUserWorkloadCheckerDailyStatExample example);
+
+    List<StatHouseQmProjectUserWorkloadCheckerDailyStat> selectByExample(StatHouseQmProjectUserWorkloadCheckerDailyStatExample example);
 }
