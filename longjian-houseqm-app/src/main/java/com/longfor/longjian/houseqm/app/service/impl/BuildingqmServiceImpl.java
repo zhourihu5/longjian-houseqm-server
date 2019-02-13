690994d3fb22f8daa30d4b597ae5cfd24fab0d5f
ds <572605334@qq.com>
2019-02-13 15:59:46 +0800

    修改 /buildingqm/v3/papi/task/edit/ 接口

--- longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/service/impl/BuildingqmServiceImpl.java
+++ longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/service/impl/BuildingqmServiceImpl.java
@@ -427,17 +427,19 @@ public class BuildingqmServiceImpl implements IBuildingqmService {
                     continue;
                 }
                 //    # 将此记录标记为不需要删除
-                UserInHouseQmCheckTask dbItem = squadUserMap.get(squadId).get(userIds.get(j));
-                doNotNeedDeleteSquaduserPkId.put(dbItem.getId(), true);
-                if (!dbItem.getCanApprove().equals(canApprove) ||
-                        !dbItem.getCanDirectApprove().equals(canDirectApprove) ||
-                        !dbItem.getCanReassign().equals(canReassign)
-                ) {
-                    dbItem.setCanApprove(canApprove);
-                    dbItem.setCanDirectApprove(canDirectApprove);
-                    dbItem.setCanReassign(canReassign);
-                    needUpdateCheckTaskSquadUser.add(dbItem);
-                }
+                        if(map!=null){
+                            UserInHouseQmCheckTask dbItem = map.get(userIds.get(j));
+                            doNotNeedDeleteSquaduserPkId.put(dbItem.getId(), true);
+                            if (!dbItem.getCanApprove().equals(canApprove) ||
+                                    !dbItem.getCanDirectApprove().equals(canDirectApprove) ||
+                                    !dbItem.getCanReassign().equals(canReassign)
+                            ) {
+                                dbItem.setCanApprove(canApprove);
+                                dbItem.setCanDirectApprove(canDirectApprove);
+                                dbItem.setCanReassign(canReassign);
+                                needUpdateCheckTaskSquadUser.add(dbItem);
+                            }
+                        }
             }
 
         }
