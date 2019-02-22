23b15de71fc761b2e868d7cd1db9903b7c2cc4a4
zhourihu <zhourihu@anhao.cn>
2019-02-22 15:09:12 +0800

    fix bug: 6.http://api.longfor.sit/longjian-houseqm-app-sit/setting/v3/papi/buildingqm/get_issuefiled_setting/ header:
    token: ZdHvQjSHHDxa7JLLZOuWooXbwxy6yVfnzizdWrSW0CWO7aO9Bqf4Gx8AE_RJW5vm
    
    x-gaia-api-key: 8847f600-0df4-4088-982d-f884ca35bc96
    
    body:
    
    device_id=866089039266156&project_ids=5,6,18,19,20,21,23,24,25,27,31,54,55,64,66,67,69,70,71,75,81,82,83,85,87,88,92,93,95,96,97,98,100,101,102,104,117,119,121,124,141,142,143,145,146,147,148,149,150,152,153,154,162,163,166,167,168,173,174,175,176,178,180,181,183,186,187,196,197,198,199,200,201,202,203,204,205,206,208,209,210,214,216,217,218,219,221,222,236,249,252,253,254,269,279,280,286,287,288,290,291,293,294,303,304,306,308,309,318,320,322,323,325,330,335,336,338,339,341,349,362,363,372,373,375,389,393,394,395,401,402,410,411,413,415,417,418,419,420,421,422,424,425,426,442,443,444,445,450,451,452,453,454,455,456,457,458,459,460,462,475,476,477,482,506,509,513,517,518,519,521,522,526,532,533,535,536,538,539,541,542,543,544,545,546,547,550,557,560,561,562,563,564,565,566,567,568,569,570,572,574,576,578,579,581,582,583,584,585,586,587,588,589,592,595,596,599,600,601,602,603,604,605,606,607,608,609,614,618,619,621,626,627,628,629,634,635,636,638,639,640,641,642,643,644,645,646,647,649,650,651,652,653,654,655,656,658,659,660,661,662,664,665,669,672,673,677,678,679,680,681,682,683,684,685,686,688,689,690,691,693,724,729,730,731,749,756,757,758,759,760,761,765,769,770,772,773,774,777,778,785,786,787,788,789,790,791,792,793,794,796,797,807,808,809,811,812,813,814,815,816,819,820,821,823,826,827,833,835,836,837,838,839,840,851,853,854,855,856,858,859,860,861,863,864,865,866,867,869,870,873,874,876,877,879,880,881,882,883,885,886,887,888,889,890,892,893,895,896,897,898,899,900,901,902,903,904,905,909,910,911,912,913,914,915,916,917,918,919,920,921,922,923,924,927,928,929,930,931,932,933,934,935,936,937,938,939,940,941,942,943,945,947,948,950,951,952,953,954,955,956,957,958,959,960,961,962,963,964,965,966,967,971,977,978,979,980,1000,1001,1005,1008,1010,1012,1021,1022,1023,1024,1025,1026,1027,1028,1029,1030,1031,1036,1037,1038,1039,1040,1041,1042,1045&token=IZriiWVKbt9JsExIwqW-EmWWCh_MtE6bjoeHa5U0DRS3GEa0ulPJvGo9miFPLsZG&
    
    错误:
    
    {
        "result": 1,
        "code": 1,
        "data": null,
        "message": "Index: 450, Size: 10",
        "timestamp": 1550816142,
        "dataClazz": 1,
        "baseDateClazz": null
    }
    
    状态：已解决
    解决人：周日虎
    Bug原因：下标取错了，数组越界
    解决办法：改为正确的index。

--- longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/service/BuildingqmSettingService.java
+++ longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/service/BuildingqmSettingService.java
@@ -63,14 +63,15 @@ public class BuildingqmSettingService {
             } else {
                 List<IssueFieldSetting> issueFieldSettings = initDefaultSetting(projectIdList.get(i));
                 for (int j = 0; j < issueFieldSettings.size(); j++) {
+                    IssueFieldSetting issueFieldSetting=issueFieldSettings.get(j);
                     ApiIssueFiledSettingMsg msg = new ApiIssueFiledSettingMsg();
-                    msg.setProject_id(issueFieldSettings.get(i).getProjectId());
-                    msg.setField_id(issueFieldSettings.get(i).getFieldId());
-                    msg.setName(issueFieldSettings.get(i).getName());
-                    msg.setAlias(issueFieldSettings.get(i).getAlias());
-                    msg.setDisplay_status(issueFieldSettings.get(i).getDisplayStatus());
-                    msg.setRequired_status(issueFieldSettings.get(i).getRequiredStatus());
-                    msg.setAlias_status(issueFieldSettings.get(i).getAliasStatus());
+                    msg.setProject_id(issueFieldSetting.getProjectId());
+                    msg.setField_id(issueFieldSetting.getFieldId());
+                    msg.setName(issueFieldSetting.getName());
+                    msg.setAlias(issueFieldSetting.getAlias());
+                    msg.setDisplay_status(issueFieldSetting.getDisplayStatus());
+                    msg.setRequired_status(issueFieldSetting.getRequiredStatus());
+                    msg.setAlias_status(issueFieldSetting.getAliasStatus());
                     issueFileds.add(msg);
                 }
 
