730e0320bf27e8c47ad927b6c4273de24fc8ff72
zhourihu <zhourihu@anhao.cn>
2019-02-28 18:37:07 +0800

    fix bug: chinese filename encode mess

--- longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/buildingqmv3papi/BuildingqmController.java
+++ longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/buildingqmv3papi/BuildingqmController.java
@@ -409,4 +409,38 @@ public class BuildingqmController {
 //        return ljBaseResponse;
             return null;
     }
+    @RequestMapping(value = "test", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public void testFileNameEncode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        //todo fix bug chinese charactor encoding in diferent browser
+        String fileNames= "测试汉字文件名.txt";
+        String codedfilename = URLEncoder.encode(fileNames, "utf-8");
+            String agent = request.getHeader("USER-AGENT");
+            System.out.println("browser agent==" + agent);
+            if (null != agent && -1 != agent.indexOf("MSIE") || null != agent && -1 != agent.indexOf("Trident")) {// ie
+                System.out.println("ie");
+                String name = java.net.URLEncoder.encode(fileNames, "UTF-8");
+                codedfilename = name;
+            } else if (null != agent && -1 != agent.indexOf("Mozilla")) {// 火狐,chrome等
+                System.out.println("Mozilla");
+                if(null != agent && -1 != agent.indexOf("Chrome")){
+                    System.out.println("Chrome");
+                }else if(null != agent && -1 != agent.indexOf("Firefox")){
+                    System.out.println("Firefox");
+                }
+                codedfilename = new String(fileNames.getBytes("UTF-8"), "iso-8859-1");
+            } else {
+                System.out.println("other browser");
+                //String fileName = String.format("%s_问题详情_%s.xlsx", category_name, dt);
+//                codedfilename = "test.txt";
+            }
+
+        response.addHeader("Content-Disposition",
+                "attachment;filename=" + codedfilename);
+
+        response.addHeader("Content-Type", "application/vnd.ms-excel; charset=utf-8");
+        response.addHeader("Expires", "0");
+        String content="你好，内容是汉字。Hello,this is english";
+        response.getOutputStream().write(content.getBytes());
+    }
 }
