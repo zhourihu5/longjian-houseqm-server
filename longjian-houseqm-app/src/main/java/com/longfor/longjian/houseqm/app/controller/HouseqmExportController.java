4cdd3a917b2e3ffab3934dde1a4545daa1a9416d
wangchenguang <wangchenguang@longfor.com>
2019-03-01 13:02:53 +0800

    smell

--- longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/HouseqmExportController.java
+++ longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/controller/HouseqmExportController.java
@@ -45,23 +45,21 @@ public class HouseqmExportController {
      **/
     @GetMapping(value = "project_orders",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public LjBaseResponse projectOrders(ProjectOrdersReq req, HttpServletResponse resp){
-        //todo 鉴权 String[] perms=new String[]{"项目.移动验房.问题管理.编辑","项目.工程检查.问题管理.编辑"};
         //ctrlTool.projPermMulti(request,perms);
 
-        Date begin_on=null,end_on=null;
+        Date beginOn=null,endOn=null;
         if (!req.getBegin_on().equals("")){
-            begin_on=DateUtil.strToDate(req.getBegin_on(),"yyyy-MM-dd");
+            beginOn=DateUtil.strToDate(req.getBegin_on(),"yyyy-MM-dd");
         }
         if (!req.getEnd_on().equals("")){
-            end_on=DateUtil.strToDate(req.getEnd_on(),"yyyy-MM-dd");
+            endOn=DateUtil.strToDate(req.getEnd_on(),"yyyy-MM-dd");
         }
         List<Integer> areaIds = StringSplitToListUtil.strToInts(req.getArea_ids(), ",");
-        String filePath=iHouseqmExportService.exportProjectOrdersByProjIdTaskIdAreaIdsRepairedIdBeginOnEndOn(req.getProject_id(),req.getTask_id(),areaIds,req.getRepairer_id(),begin_on,end_on,req.getCategory_cls(),false);
+        iHouseqmExportService.exportProjectOrdersByProjIdTaskIdAreaIdsRepairedIdBeginOnEndOn(req.getProject_id(),req.getTask_id(),areaIds,req.getRepairer_id(),beginOn,endOn,req.getCategory_cls(),false);
 
         resp.setHeader("Content-Type", "application/octet-stream; charset=utf-8");
         resp.setHeader("Content-Disposition", " attachment; filename=\" 工程处理单.zip\"");
         resp.setHeader("Expires", "0");
-        //todo	c.File(filePath)
 
         return null;
     }
