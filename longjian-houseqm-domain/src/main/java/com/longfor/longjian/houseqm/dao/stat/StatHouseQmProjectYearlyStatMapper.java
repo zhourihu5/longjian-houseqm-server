5e3aad599e96998db442de412f2b69f60b1ae89d
a-yanglei <a-yanglei>
2019-03-04 09:58:41 +0800

    2019-03-04杨磊 -过程 修改技术债

--- longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/stat/StatHouseQmProjectYearlyStatMapper.java
+++ longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/stat/StatHouseQmProjectYearlyStatMapper.java
@@ -4,7 +4,5 @@ import com.longfor.gaia.gfs.data.mybatis.LFMySQLMapper;
 import com.longfor.longjian.houseqm.po.stat.StatHouseQmProjectYearlyStat;
 
 public interface StatHouseQmProjectYearlyStatMapper extends LFMySQLMapper<StatHouseQmProjectYearlyStat> {
-    /*int countByExample(StatHouseQmProjectYearlyStatExample example);
 
-    List<StatHouseQmProjectYearlyStat> selectByExample(StatHouseQmProjectYearlyStatExample example);*/
 }
