10225e20d8b3eb545c8db40b551318b0dffb01b7
a-yanglei <a-yanglei>
2019-03-05 19:09:22 +0800

    2019-03-05 过程 单元测试开发 杨磊

---
+++ longjian-houseqm-app/test/main/java/com/longfor/longjian/houseqm/app/service/test/BuildingqmSettingServiceTest.java
@@ -0,0 +1,33 @@
+package com.longfor.longjian.houseqm.app.service.test;
+
+import com.longfor.longjian.houseqm.Application;
+import com.longfor.longjian.houseqm.app.service.BuildingqmSettingService;
+import com.longfor.longjian.houseqm.util.DateUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class},webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class BuildingqmSettingServiceTest {
+
+    @Resource
+    private BuildingqmSettingService buildingqmSettingService;
+
+
+    @Test
+    public void getIssuefiledSetting() {
+        try{
+            buildingqmSettingService.getIssuefiledSetting("1,2,3,4,5,6,7,8,9,10", DateUtil.datetimeToTimeStamp(new Date()));
+        }catch (Exception e){
+            Assert.fail(e.getMessage());
+        }
+    }
+}
