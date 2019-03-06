2625704a00bb9c83426e1c0a3e2c2e136e4291b7
houyan <615786504@qq.com>
2019-03-06 20:50:21 +0800

    修改 sonar 2

--- longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/utils/CtrlToolUtils.java
+++ longjian-houseqm-app/src/main/java/com/longfor/longjian/houseqm/app/utils/CtrlToolUtils.java
@@ -5,6 +5,10 @@ import com.longfor.longjian.common.base.LjBaseResponse;
 
 public class CtrlToolUtils {
 
+    public CtrlToolUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static <T> LjBaseResponse<T> errorReturn(LjBaseResponse<T> response, Exception e){
         response.setMessage(e.getMessage());
         response.setResult(1);
