d321ee57bbade8caee8de805fe155a9e7fe8f863
houyan <615786504@qq.com>
2019-03-08 18:15:59 +0800

    解决冲突

--- longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/util/TokenGetUtil.java
+++ longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/util/TokenGetUtil.java
@@ -4,6 +4,6 @@ package com.longfor.longjian.houseqm.util;
 public class TokenGetUtil {
 
     public static String getToken(){
-        return "uI9mOcMbkh6VIN3S2EPfYqYGYeFlVxenYsE6IW3GNJt16im8CmNnhFc2-a0O8q5-";
+        return "gyoC6oxaBFZxP4LJrX9Jaqs2W2aPCprmNC-DcymXOJUl5vQN_d9KHcRR7O597Srh";
     }
 }
