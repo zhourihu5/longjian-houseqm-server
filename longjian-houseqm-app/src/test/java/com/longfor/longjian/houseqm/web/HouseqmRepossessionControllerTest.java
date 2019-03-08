8fdefbf3ebabcbb1d47412b99d067b5e1dc2e9e7
ds <572605334@qq.com>
2019-03-08 20:54:14 +0800

    sonar 单元测试

---
+++ longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/web/HouseqmRepossessionControllerTest.java
@@ -0,0 +1,62 @@
+package com.longfor.longjian.houseqm.web;
+
+import com.longfor.longjian.common.filter.UrlFilter;
+import com.longfor.longjian.houseqm.Application;
+import com.longfor.longjian.houseqm.util.TokenGetUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by Dongshun on 2019/3/8.
+ */
+@RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！
+@SpringBootTest(classes = Application.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) // 指定我们SpringBoot工程的Application启动类
+public class HouseqmRepossessionControllerTest {
+    private static final String TOKEN = TokenGetUtil.getToken();
+    private MockMvc mockMvc;
+    @Autowired
+    protected WebApplicationContext wac;
+    @Before()
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).addFilter(new UrlFilter()).build();
+    }
+
+/*
+    @Test
+    public void testreport() throws Exception {
+        mockMvc.perform(
+                post("/houseqm/v3/papi/repossession/report").header("token",TOKEN)
+                        .param("data","[{\"project_id\":3,\"task_id\":291,\"area_id\":32,\"uuid\":\"asdfghjkl\"}]")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("success"))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+    }
+
+*/
+
+
+    @Test
+    public void testGet() throws Exception {
+        mockMvc.perform(
+                post("/houseqm/v3/papi/repossession/get").header("token",TOKEN)
+                        .param("task_ids","291") .param("timestamp","1") .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("success"))
+                .andDo(MockMvcResultHandlers.print()).andReturn();
+    }
+
+}
