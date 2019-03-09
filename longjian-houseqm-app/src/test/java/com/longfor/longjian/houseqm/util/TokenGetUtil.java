1b88411ea2f0a1a86e497d34e2237c67d4529a1f
houyan <615786504@qq.com>
2019-03-09 16:48:55 +0800

    修改token

--- longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/util/TokenGetUtil.java
+++ longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/util/TokenGetUtil.java
@@ -4,6 +4,6 @@ package com.longfor.longjian.houseqm.util;
 public class TokenGetUtil {
 
     public static String getToken(){
-        return "3bANO5HgAAs2t8Sju6mVqIXz-SE-Gs94erSuhrVDvXTFfcblvz8TZSKMNKpt4JvA";
+        return "gwG8kz6nSJhu1NE8yH_Ty2EDrneIKiFFbJw2EipTBTCcZnoJEIwsDfAAnSy62kji";
     }
 }
