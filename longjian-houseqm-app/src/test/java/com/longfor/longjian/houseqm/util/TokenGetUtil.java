f3d961203f558903f1ef3d9c0c297d5df069bd95
houyan <615786504@qq.com>
2019-03-09 13:53:44 +0800

    单元测试

--- longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/util/TokenGetUtil.java
+++ longjian-houseqm-app/src/test/java/com/longfor/longjian/houseqm/util/TokenGetUtil.java
@@ -4,6 +4,6 @@ package com.longfor.longjian.houseqm.util;
 public class TokenGetUtil {
 
     public static String getToken(){
-        return "UH9jrZ5xKgkhpbx6FkOi0Gj_gZx2RY1H0e6yEkECi_JisSDQhQL8OJ-rNPZCKd0e";
+        return "7CnqIdxDA2PCgR_5P5QWT0QzIB71JN8f7pOKDf7HGJ2wB1Acke08nLs0qShQJd_d";
     }
 }
