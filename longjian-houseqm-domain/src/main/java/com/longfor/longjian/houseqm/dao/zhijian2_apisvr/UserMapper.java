7305d032ccca3bb37f77430d449ca65a747d9964
houyan <615786504@qq.com>
2019-03-14 10:21:39 +0800

    修改 report_issue 1

--- longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/zhijian2_apisvr/UserMapper.java
+++ longjian-houseqm-domain/src/main/java/com/longfor/longjian/houseqm/dao/zhijian2_apisvr/UserMapper.java
@@ -2,12 +2,7 @@ package com.longfor.longjian.houseqm.dao.zhijian2_apisvr;
 
 import com.longfor.gaia.gfs.data.mybatis.LFMySQLMapper;
 import com.longfor.longjian.houseqm.po.zhijian2_apisvr.User;
-import org.apache.ibatis.annotations.Param;
-
-import java.util.List;
 
 public interface UserMapper extends LFMySQLMapper<User> {
-
-    List<User> selectByUserIdInAndNoDeleted(@Param("userIds") List<Integer> userIds, @Param("deleted") String deleted);
 
 }
